fe29bce1ad4cc27ef93948fc3e9d294214f957fd
eilla1 <git@ella.cx>
2021-12-26 17:29:57 -0800

    It no work.

--- js/doggo.js
+++ js/doggo.js
@@ -1,11 +1,11 @@
 const canvas = document.querySelector("canvas");
-const context = canvas.getContext("2d");
+const ctx = canvas.getContext("2d");
 
 const imgDimensions = { width: 107, height: 155.5 };
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
-context.translate(window.innerWidth / 2, window.innerHeight / 2);
+ctx.translate(window.innerWidth / 2, window.innerHeight / 2);
 
 const img = new Image();
 img.src = "./static/trigger.png";
@@ -14,12 +14,21 @@ img.onload = () => {
   draw();
 };
 
-function draw() {
-  context.drawImage(
+function draw(offset) {
+  console.log("draw " + img);
+  ctx.drawImage(
     img,
-    -imgDimensions.width / 2,
-    -imgDimensions.height / 2,
-    imgDimensions.width,
-    imgDimensions.height
+    -imgDimensions.width / 2 - offset / 2,
+    -imgDimensions.height / 2 - offset / 2,
+    imgDimensions.width + offset,
+    imgDimensions.height + offset
   );
+}
+draw(2);
+const loopCount = 30;
+
+function loopDraw() {
+  for (let i = loopCount; i >= 0; i--) {
+    draw(i);
+  }
 }
